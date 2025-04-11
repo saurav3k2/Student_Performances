a4f8a31be437eb71daa5c16dba02774d384e7490
saurav3k2 <saurav3k2@gmail.com>
2025-04-11 11:21:14 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages , setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    
+    """This function will return the list of requirements"""
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        requirements=[req.replace("\n"  ,"") for req in requirements ]
+        
+        
+        if HYPEN_E_DOT in  requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+        
+        
+        return requirements
+    
+        
+        
+
+setup(
+    name = "Student_Performance",
+    version = "0.0.1",
+    author = "Saurav",
+    author_email="saurav3k2@gmail.com",
+    install_requires = get_requirements('requirements.txt'),
+    
+    packages = find_packages(),
+)
